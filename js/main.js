339a77370ccb5b9f091c50910551ad9b2a34b3fe
baxromov <baxromov.shahzodbek@gmail.com>
2020-11-28 23:18:58 +0500

    Add some sections

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var owl = $('.owl-carousel');
+
+owl.owlCarousel({
+  loop: true,
+  autoplay: true,
+  items: 1,
+  nav: true,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+})
