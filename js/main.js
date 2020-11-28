edee5dd013a75ca453ce09473025a6f80c4f8fd4
baxromov <baxromov.shahzodbek@gmail.com>
2020-11-29 00:47:55 +0500

    ass

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
-var owl = $('.owl-carousel');
-
+var owl = $('#owl-carousels');
+var team = $('#team-carousel');
 owl.owlCarousel({
   loop: true,
   autoplay: true,
   items: 1,
-  nav: true,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
-})
+  nav: true,
+});
+
+team.owlCarousel({
+  loop:true,
+  autoplay:true,
+  items:3,
+  nav:true,
+  dots:true,
+  center:true,
+  margin:10,
+  navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+  responsive:{
+    0 : {
+      items:1,
+    },
+    480 : {
+      items:1,
+    },
+    768 : {
+      items:3,
+    }
+  }
+});
